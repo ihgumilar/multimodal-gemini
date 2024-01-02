9d6e867d3d5d3505d523cc0af56770f2552b7f12
ihgumilar <igum002@aucklanduni.ac.nz>
2024-01-03 11:34:14 +1300

    Upload file successfully, give prompt, and stream response into chatinterface
    But not yet able to detect a new uploaded picture file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # ---
 # jupyter:
 #   jupytext:
-#     cell_metadata_filter: -all
+#     cell_metadata_filter: title,-all
 #     custom_cell_magics: kql
 #     text_representation:
 #       extension: .py
@@ -15,96 +15,83 @@
 #     name: python3
 # ---
 
+# %% [markdown]
+# # Libraries
+
 # %%
 import os
 import time
 
-# %%
 import google.generativeai as genai
 import gradio as gr
 import PIL.Image
 from llama_index.multi_modal_llms.gemini import GeminiMultiModal
 from llama_index.schema import ImageDocument
 
-# %%
-# # Configure Gemini Pro Vision
-# genai.configure(api_key='AIzaSyDP9Wv8Q-2vRJFQge-zyO138qdokEUMe3k')
-# model = genai.GenerativeModel('gemini-pro-vision')
+# %% [markdown]
+# # Setup and configuration
 
-
+# %% Configuration
 GOOGLE_API_KEY='AIzaSyDP9Wv8Q-2vRJFQge-zyO138qdokEUMe3k'  # add your GOOGLE API key here
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
 
-# %%
-# # Function to generate content using Gemini Pro Vision
-# def generate_content(history, prompt, file_path):
-#     img = PIL.Image.open(file_path)
-#     response = model.generate_content([prompt, img])
-#     generated_text = response.text
-
-#     # Update the chat history with the generated text
-#     history[-1][1] = generated_text
-
-#     return history
+# %% [markdown]
+# # Functions
 
 # %%
-# Gradio code
+all_image_documents = []
+
 def print_like_dislike(x: gr.LikeData):
     print(x.index, x.value, x.liked)
 
-# %%
 def add_text(history, text):
     history = history + [(text, None)]
     return history, gr.Textbox(value="", interactive=False)
     
-
-
-# %%
 def add_file(history, file):
     history = history + [((file.name,), None)]
     return history
 
-
-
-# %%
-def bot_text(history):
-    response = "**That's cool!**"
-    history[-1][1] = ""
-    for character in response:
-        history[-1][1] += character
-        time.sleep(0.05)
-        yield history
-
-def bot_picture(history):
-    # history[-1][1] = ""
-    # for character in history[0][0]:
-    #     history[-1][1] += character
-    #     time.sleep(0.05)
-    #     yield history
-
+def store_image(history):
     # Get the uploaded file
     image_path = history[0][0][0]
     print(image_path)
-    prompt = "Describe the given picture"
-    image_documents = [
-      ImageDocument(image_path=image_path
-      )
-    ]
+    all_image_documents.append(ImageDocument(image_path=image_path))
 
+    status = "Uploaded successfuly"
+
+    history[-1][1] = ""
+    for character in status:
+        history[-1][1] += character
+        time.sleep(0.01)
+        yield history
+
+
+def bot_text(history):
+    
+    prompt = history[-1][0]
+    
     # Process with Gemini pro-vision
     gemini_pro = GeminiMultiModal(model_name="models/gemini-pro-vision")
     complete_response = gemini_pro.complete(
         prompt=prompt,
-        image_documents=image_documents,
+        image_documents=all_image_documents,
     )
 
     history[-1][1] = ""
-    print(f"complete_response {complete_response}-{type(complete_response)}")
+    # print(f"complete_response {complete_response}-{type(complete_response)}")
     for character in complete_response.text:
         history[-1][1] += character
         time.sleep(0.05)
         yield history
+
+
+
+
+
+# %% [markdown]
+# # Gradio main block
 
 # %%
 with gr.Blocks() as demo:
@@ -124,16 +111,21 @@ with gr.Blocks() as demo:
         )
         btn = gr.UploadButton("📁", file_types=["image", "video", "audio"])
 
+    # Submit text
     txt_msg = txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
         bot_text, chatbot, chatbot, api_name="bot_response"
     )
     txt_msg.then(lambda: gr.Textbox(interactive=True), None, [txt], queue=False)
+
+    # Upload picture
+    # file_msg = btn.upload(add_file, [chatbot, btn], [chatbot], queue=False).then(
+    #     bot_picture, chatbot, chatbot
+    # )
     file_msg = btn.upload(add_file, [chatbot, btn], [chatbot], queue=False).then(
-        bot_picture, chatbot, chatbot
+        store_image, chatbot, chatbot
     )
 
     chatbot.like(print_like_dislike, None, None)
-
 
 
 
